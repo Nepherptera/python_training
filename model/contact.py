e84dbfe7e1eab59ea356ccb54c0e3d6514c48d18
Julianna Smirnova <yannit@inbox.ru>
2022-10-07 21:50:36 +0300

    Клеим строки из частей: прямые и обратные проверки

--- model/contact.py
+++ model/contact.py
@@ -2,7 +2,11 @@ from sys import maxsize
 
 class Contact:
 
-    def __init__(self, firstname=None, middlename=None, lastname=None, nickname=None, title=None, company=None, address=None, home=None, mobile=None, work=None, fax=None, email=None, email2=None, email3=None, homepage=None, bday=None, bmonth=None, byear=None, aday=None, amonth=None, ayear=None, address2=None, phone2=None, notes=None, id=None):
+    def __init__(self, firstname=None, middlename=None, lastname=None, nickname=None, title=None, company=None,
+                 address=None, home=None, work=None, mobile=None, fax=None,
+                 email=None, email2=None, email3=None, homepage=None,
+                 bday=None, bmonth=None, byear=None, aday=None, amonth=None, ayear=None,
+                 address2=None, phone2=None, notes=None, id=None, all_phones_from_homepage=None):
         self.firstname = firstname
         self.middlename = middlename
         self.lastname = lastname
@@ -11,8 +15,8 @@ class Contact:
         self.company = company
         self.address = address
         self.home = home
-        self.mobile = mobile
         self.work = work
+        self.mobile = mobile
         self.fax = fax
         self.email = email
         self.email2 = email2
@@ -28,6 +32,7 @@ class Contact:
         self.phone2 = phone2
         self.notes = notes
         self.id = id
+        self.all_phones_from_homepage = all_phones_from_homepage
 
     def __repr__(self):
         return "%s:%s:%s" % (self.id, self.lastname, self.firstname)
